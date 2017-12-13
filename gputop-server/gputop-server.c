a2154add1e6fb2965dba355595fed67684b7c2b2
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2017-12-13 16:00:36 +0000

    server: report available perf events through protocol

--- gputop-server/gputop-server.c
+++ gputop-server/gputop-server.c
@@ -1255,6 +1255,13 @@ handle_get_features(h2o_websocket_conn_t *conn,
              pb_features.n_tracepoints++) {
         }
     }
+    pb_features.events = gputop_get_events_names();
+    if (pb_features.events) {
+        for (pb_features.n_events = 0;
+             pb_features.events[pb_features.n_events];
+             pb_features.n_events++) {
+        }
+    }
 
     gputop_read_file("/proc/sys/kernel/osrelease", kernel_release, sizeof(kernel_release));
     gputop_read_file("/proc/sys/kernel/version", kernel_version, sizeof(kernel_version));
@@ -1300,6 +1307,7 @@ handle_get_features(h2o_websocket_conn_t *conn,
     send_pb_message(conn, &pb_message.base);
 
     gputop_debugfs_free_tracepoint_names(pb_features.tracepoints);
+    gputop_free_events_names(pb_features.events);
 
 #ifdef SUPPORT_GL
     if (pb_features.n_gl_queries)
